c94f1b0765c3405171286386a0cf16ba52d9123f
SAMUEL LUCIANO CORREIA DA CUNHA <8160526@estg.ipp.pt>
2020-09-22 11:24:45 +0100

    start of learning vetores

---
+++ Course/Vetores/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.company.vetor.Vetor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+    }
+}

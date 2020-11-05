dcf99fffeae34afceee7a0af14f42faabd4f073f
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-05 19:32:49 +0000

    minor change

--- FP5/Exercicio 1 SemModCount/src/OrderedList.java
+++ FP5/Exercicio 1 SemModCount/src/OrderedList.java
@@ -13,30 +13,32 @@ public class OrderedList<T extends Comparable> extends ArrayList<T> implements O
      */
     @Override
     public void add(T element) {
-        if (isEmpty()) {
-            array[0] = element;
-        } else if (size() == array.length) {
-            expandCapacity();
-        } else {
-            boolean found = false;
-            int current = 0;
-
-            while (current < size && found == false) {
-                Comparable<T> tmp = (Comparable<T>) array[current];
-                if (tmp.compareTo(element) > 0) {
-                    found = true;
-                } else {
-                    current++;
-                }
-            }
-
-            if(found = true){
-                for (int i = rear - 1; i >= current; i--) {
-                    array[i + 1] = array[i];
-                }
-                array[current] = element;
+        if (element instanceof Comparable) {
+            if (isEmpty()) {
+                array[0] = element;
+            } else if (size() == array.length) {
+                expandCapacity();
             } else {
-                array[rear] = element;
+                boolean found = false;
+                int current = 0;
+
+                while (current < size && found == false) {
+                    Comparable<T> tmp = (Comparable<T>) array[current];
+                    if (tmp.compareTo(element) > 0) {
+                        found = true;
+                    } else {
+                        current++;
+                    }
+                }
+
+                if(found = true){
+                    for (int i = rear - 1; i >= current; i--) {
+                        array[i + 1] = array[i];
+                    }
+                    array[current] = element;
+                } else {
+                    array[rear] = element;
+                }
             }
         }
         rear++;

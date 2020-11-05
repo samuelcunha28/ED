21df153a38060214e20a1c94cd1bd282234c83ed
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-05 17:36:49 +0000

    done except the expand capacity

---
+++ FP5/Exercicio 1 SemModCount/src/Exceptions/ComparableException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class ComparableException extends Exception {
+
+    public ComparableException(String message) {
+        super(message);
+    }
+}
--- FP5/Exercicio 1 SemModCount/src/Main.java
+++ FP5/Exercicio 1 SemModCount/src/Main.java
@@ -1,6 +1,30 @@
+import Exceptions.ElementNotFoundException;
+import Exceptions.EmptyCollectionException;
+
+import java.util.Iterator;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws ElementNotFoundException, EmptyCollectionException {
+        OrderedList<String> list = new OrderedList<>();
+        list.add("D");
+        list.add("B");
+        list.add("A");
+        list.add("C");
+        list.add("E");
+
+        list.remove("E");
+        list.removeLast();
+        list.removeFirst();
+
+
+        Iterator it = list.iterator();
+
+        while (it.hasNext()) {
+            String obj = (String) it.next();
+            System.out.println(obj);
+        }
+
+        // System.out.println(list.toString());
     }
 }

34915d048070a0749ed4c11c43cb5c1f3bc643d4
Samuel Cunha <samuellucianocorreiacunha@hotmail.com>
2020-11-19 15:00:40 +0000

    example of towers of hanoi

---
+++ FP7/Hanoi_Towers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /**
+     * Creates a TowersOfHanoi puzzle and solves it
+     */
+    public static void main(String[] args) {
+	    TowesOfHanoi towers = new TowesOfHanoi(4);
+	    towers.solve();
+    }
+}
